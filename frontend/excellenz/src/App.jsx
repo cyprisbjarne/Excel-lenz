e29136f73b8068f796a00cca8e6d6d2775781fff
InkmyTink <wildwildwildgoose@gmail.com>
2026-05-18 21:38:18 +0200

    umsatz start
    umsatz in page und style einfügen

--- frontend/excellenz/src/App.jsx
+++ frontend/excellenz/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Dashboard from "./pages/Dashboard";
 import "./styles/global.css";
+import Umsatz from "./pages/umsatz.jsx";
 
 export default function App() {
-  return <Dashboard />;
+  return <Umsatz />;
 }
---
+++ frontend/excellenz/src/pages/umsatz.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/umsatz.css";
+
+
+
+export default function Umsatz() {
+    return (
+        <h1>Hier der Text lol</h1>
+
+    );
+}
