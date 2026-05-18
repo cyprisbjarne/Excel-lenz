c2681e44c0bb4c90f1e6cc1f3c2cb54216c1bc01
InkmyTink <wildwildwildgoose@gmail.com>
2026-05-18 23:16:24 +0200

    umsatz seite
    pro Monat
    verkauf preis
    umsatzrechnung

--- frontend/excellenz/src/pages/umsatz.jsx
+++ frontend/excellenz/src/pages/umsatz.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/umsatz.css";
 
+//noch nichts wie sidebar und so eingefügt,
+//dient bis jetzt einfach nur also vorlage für den Umsatz
+//Layout, UI, noch nichts konkretes, einfach nur die basic Umsatzechnung
 
 
 export default function Umsatz() {
+
+    //Variablen sind bis jetzt nur als Beispiel mit vorgegebenen Wert
+
+    const [verkauf, setVerkauf] = useState(150);
+    const [preis, setPreis] = useState(12);
+
+    const umsatz = verkauf * preis;
+
+
     return (
-        <h1>Hier der Text lol</h1>
+        <div className="vbox">
+            <h1 className="title">Umsatz</h1>
+
+            <section>
+                <h2 className="smalltitle">pro Monat: </h2>
+                <div className="hbox">
+                    <div className="eingabe box">
+                        Verkaufseinheiten: {verkauf}
+                    </div>
+                    <div className="eingabe box">
+                        Preis: {preis}€
+                    </div>
+                </div>
+                <div className="umsatz box">
+                    Umsatz: {umsatz}€
+                </div>
+            </section>
+            <div className="platzhalter">füge hier diagramm ein</div>
+        </div>
 
     );
 }
